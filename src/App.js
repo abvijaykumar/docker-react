82fb09cb1ada51ea7b738b8ae99fedabbd082d7b
abvk <abvk.ario@gmail.com>
2019-06-09 20:38:41 -0500

    changed the code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to React in Docker, This is awesome
+          hey AB, welcome to React running on dockers on AWS
         </p>
         <a
           className="App-link"
